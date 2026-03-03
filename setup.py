ab61dcfe17a8a8e19618cb9b8ae14ffe6ac1f6ea
rajaagl <rajaa.mahroug@uit.ac.ma>
2026-03-03 00:07:28 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Rajaa Mahroug',
+    author_email="rajaamahroug0@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
